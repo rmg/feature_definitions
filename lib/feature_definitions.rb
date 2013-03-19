055926496919cc1ddf214e21199230c463099478
Ryan Graham <r.m.graham@gmail.com>
2013-03-19 09:17:09 -0700

    Refactor out instance/class variable access, add block mode

--- lib/feature_definitions.rb
+++ lib/feature_definitions.rb
@@ -2,23 +2,29 @@ require 'lazy_const'
 
 class FeatureDefinitions
   extend LazyConst
+  attr_reader :opts, :test_proc
 
   def self.define_feature(name, opts = {}, &block)
-    lazy_const(name){ new(opts, &block) }
+    lazy_const(name){ new(opts, block.to_proc) }
   end
 
-  def initialize(opts = {}, &block)
+  def initialize(opts, proc)
     @opts = opts
-    @proc = block.to_proc
+    @test_proc = proc
   end
 
   def self.context=(context)
     @@context = context
   end
 
-  def enabled?
-    args = @opts[:using].map { |sym| @@context.public_send(sym) }
-    @proc.call(*args)
+  def context
+    @@context
   end
 
+  def enabled?(&block)
+    args = opts[:using].map { |sym| context.public_send(sym) }
+    test_proc.call(*args).tap do |verdict|
+      yield if block_given? and verdict
+    end
+  end
 end
